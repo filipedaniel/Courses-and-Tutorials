3184f610eeb9830d3d9017256c6dab2d9699b1e5
Filipe Daniel <fdaniel@mbp-fdaniel.lan>
2018-04-18 20:31:57 +0100

    Final ES6;

--- Essentials-in-JavaScript-ES6/es6/app/index.js
+++ Essentials-in-JavaScript-ES6/es6/app/index.js
@@ -16,10 +16,38 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
+const apiKey = '0f960cfdf98e322943145f137ee559d9';
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      city: 'London'
+    }
+  }
+
+  componentDidMount() {
+    this.grabWeather(this.state.city);
+  }
+
+  grabWeather(city) {
+    fetch(`http://api.openweathermap.org/data/2.5/weather?APPID=${apiKey}&q=${city}`)
+      .then(response => response.json())
+      .then(json => {
+        this.setState({
+          description: json.weather[0].description
+        })
+      });
+
+    }
+
   render() {
+    // <div> React JS and JSX in action! </div>
     return (
-      <div>React JS and JSX in action!</div>
+      <div>
+        <h1>Weather Report for {this.state.city}!</h1>
+        <h2> {this.state.description} </h2>
+      </div>
     )
   }
 }
