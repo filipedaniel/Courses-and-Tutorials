69ebd7667061020f39b4992dbbd9a9ffecc6f172
Filipe Daniel <fdaniel@mbp-fdaniel.lan>
2018-08-08 19:33:37 +0100

    end varibles.p2

--- Others/Shell Scripting Tutorial/10.variables-p2/var3.sh
+++ Others/Shell Scripting Tutorial/10.variables-p2/var3.sh
@@ -5,3 +5,9 @@ echo "My name is $0"
 echo "My first parameter is $1"
 echo "My second parameter is $2"
 echo "All parameters are $@"
+
+# $# - total parameters
+# $0 - parameter index 0
+# $1 - parameter index 1
+# $2 - parameter index 2
+# $@ - all parameters
