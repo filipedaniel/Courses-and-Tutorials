130df6fdb8f73c99c7d847afc44de1ef525a4751
Filipe Ribeiro <Filipe.Ribeiro@checkmarx.com>
2020-01-29 18:04:45 +0000

    .

---
+++ courses/object-oriented-programming-fundamentals-in-c#/ACM/ACM.BL/Customer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACM.BL
+{
+    public class Customer
+    {
+        public int CustomerId { get; private set; }
+
+        public string EmailAddress { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+         
+        public string FullName
+        {
+            get
+            {
+                string fullName = "";
+
+
+                if (!string.IsNullOrWhiteSpace(LastName) && !string.IsNullOrWhiteSpace(FirstName))
+                {
+                    fullName = LastName + ", " + FirstName;
+                }
+                else
+                {
+                    fullName = !string.IsNullOrWhiteSpace(LastName) ? LastName : FirstName;
+                }
+
+                return fullName;
+            }
+        }
+    }
+}
---
+++ courses/object-oriented-programming-fundamentals-in-c#/ACM/Test/ACM.BLTest/CustomerTest.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using ACM.BL;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ACM.BLTest
+{
+    [TestClass]
+    public class CustomerTest
+    {
+        [TestMethod]
+        public void FullNameTestValid()
+        {
+            //-- Arrange
+            Customer customer = new Customer
+            {
+                FirstName = "Jhon",
+                LastName = "Doe"
+            };
+            string expected = "Doe, Jhon";
+
+            //-- Act
+            string actual = customer.FullName;
+
+            //-- Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [TestMethod]
+        public void FullNameFirstNameEmpty()
+        {
+            //-- Arrange
+            Customer customer = new Customer
+            {
+                LastName = "Doe"
+            };
+            string expected = "Doe";
+
+            //-- Act
+            string actual = customer.FullName;
+
+            //-- Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [TestMethod]
+        public void FullNameLastNameEmpty()
+        {
+            //-- Arrange
+            Customer customer = new Customer
+            {
+                FirstName = "Jhon"
+            };
+            string expected = "Jhon";
+
+            //-- Act
+            string actual = customer.FullName;
+
+            //-- Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+
+
+    }
+}
